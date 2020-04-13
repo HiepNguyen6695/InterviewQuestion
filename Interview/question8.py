514184d1b588b6f3f0352506cd0d2d4031ad153b
Kevroo Nguyen <kevroonguyen@Kevroos-MacBook-Pro.local>
2020-04-13 09:01:41 -0700

    Fourth Commit

--- Interview/question8.py
+++ Interview/question8.py
@@ -1,25 +1,14 @@
 # How are duplicates removed from a given array in Java?
-array = [12,3,44,8,4,3,4]
-array.sort()
-n = len(array)
-def remove_dup(arr, n):
-    
-    if n == 0 or n == 1: 
-        return n 
-    count = 0
-    for i in range(0, n - 1):
-        if arr[i] != arr[i+1]:
-            arr[count] = arr[i]
-            count += 1
-            #print(count)
-            #print(arr)
-        else:
-            print(arr[i])
-    arr[count] = arr[n - 1]
-    count += 1
-    return count
+array = [1,2,3,4,5,3]
 
-remove_dup(array, n)
+def remove_dup_with_lib(arr):
+    newSet = set()
+    for num in arr:
+        if not newSet.__contains__(num):
+            newSet.add(num)
+    print("Original Array")
+    print(arr)
+    print("Duplicates Removed")
+    print(list(newSet))
 
-#for i in range(0, n):
-#    print(array[i])
+remove_dup_with_lib(array)
